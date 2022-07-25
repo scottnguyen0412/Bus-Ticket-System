041c903c177e3f04402870422feebe61dc87cef0
scottnguyen0412 <scottnguyen0412@gmail.com>
2022-07-25 18:09:30 +0700

    #22 add login and register

--- bus-management/app/Providers/AppServiceProvider.php
+++ bus-management/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Mỗi khi migration thì chuỗi của kiểu dữliệu có độ dài là 191 
+        Schema::defaultStringLength(191);
     }
 }
