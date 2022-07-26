85225a7126476b1407b17f740010ee6ea82f1bf6
scottnguyen0412 <scottnguyen0412@gmail.com>
2022-07-26 15:47:40 +0700

    #26 - handle role with middleware

--- bus-management/database/seeders/DatabaseSeeder.php
+++ bus-management/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Role::firstOrCreate(['name' => Role::ROLE_ADMIN]);
+        Role::firstOrCreate(['name' => Role::ROLE_USER]);
+        Role::firstOrCreate(['name' => Role::ROLE_DRIVER]);
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- bus-management/routes/web.php
+++ bus-management/routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\Account\AccountController;
+use App\Http\Controllers\Auth\LoginController;
+
+
 
 
 
@@ -23,11 +27,21 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+// Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::prefix('admin')->group(function(){
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-    Route::get('/account',[AccountController::class,'index'])->name('admin.account.index');
+// Route::prefix('admin')->group(function(){
+//     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+//     Route::get('/account',[AccountController::class,'index'])->name('admin.account.index');
+
+// });
+
+Route::group(['middleware' => 'auth'], function () {
+    // Note: 'role:admin,driver' not have any space between
+    Route::group(['prefix' => 'admin', 'middleware' => 'role:admin,driver'], function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+        Route::get('/account',[AccountController::class,'index'])->name('admin.account.index');
+    });
 
 });
