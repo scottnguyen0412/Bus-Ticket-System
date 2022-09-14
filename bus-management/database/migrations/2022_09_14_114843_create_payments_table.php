99103e5fc3dfebd77fd698cfdc6c6d5e4e113a44
scottnguyen0412 <scottnguyen0412@gmail.com>
2022-09-14 18:56:00 +0700

    #47 Migration payments table and create model paymennt

---
+++ bus-management/database/migrations/2022_09_14_114843_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('booking_id');
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+            $table->float('total');
+            $table->dateTime('payment_date');
+            $table->string('payment_method');
+            $table->string('payment_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
